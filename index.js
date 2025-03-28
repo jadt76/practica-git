456608dfd19b5e53a1b78437db8e5bfee4b2c642
Juan Martinez <juanmartinezt59@gmail.com>
2025-03-28 03:34:26 -0300

    Subiendo solo js

---
+++ index.js
@@ -0,0 +1,20 @@
+const multiplicar = (x, y) => { return `${x * y}`; };
+const imprimir = (x) => { return `${x}`; };
+
+function calcularMultiplicacion(a, b) {
+    let resultado = multiplicar(a, b);
+
+    console.log("Resultado de la multiplicación: ", resultado);
+
+    imprimir(resultado);
+}
+
+calcularMultiplicacion(3, 7);
+// Resultado de la multiplicación: 21
+
+
+
+
+/*
+ocon.santiago@gmail.com
+*/
